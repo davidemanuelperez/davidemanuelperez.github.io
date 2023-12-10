a15695673b5a3b8b1e9f380b42fa57d949573286
davidemanuelperez <144061531+davidemanuelperez@users.noreply.github.com>
2023-12-10 15:34:07 -0500

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ let answered = false
 
 
 
-    x=localStorage.getItem("x") || 0
+    x=localStorage.getItem("x")
     upgr=localStorage.getItem("upgr") || 1
     lvl=localStorage.getItem("lvl") || 1
     xp=localStorage.getItem("xp") || 0
