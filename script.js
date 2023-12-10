ac89efada24171cc219b96cf9dd4577eee922752
davidemanuelperez <144061531+davidemanuelperez@users.noreply.github.com>
2023-12-10 16:18:55 -0500

    Update script.js

--- script.js
+++ script.js
@@ -76,24 +76,6 @@ const refiners = [
     {refiner: 32, cost: 10000000000000000}
 ]
 
-function setCookie(name, value) {
-  document.cookie = name + "=" + value + "; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/";
-}
-
-function getCookie(name) {
-  var cookieName = name + "=";
-  var cookieArray = document.cookie.split(';');
-  for(var i = 0; i < cookieArray.length; i++) {
-    var cookie = cookieArray[i];
-    while(cookie.charAt(0) === ' ') {
-      cookie = cookie.substring(1, cookie.length);
-    }
-    if(cookie.indexOf(cookieName) === 0) {
-      return cookie.substring(cookieName.length, cookie.length);
-    }
-  }
-  return null;
-}
 
 window.onload = function() {
   x = parseInt(getCookie("x")) || 0;
@@ -118,6 +100,25 @@ window.onbeforeunload = function() {
   setCookie("rebirths", rebirths);
 }
 
+function setCookie(name, value) {
+  document.cookie = name + "=" + value + "; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/";
+}
+
+function getCookie(name) {
+  var cookieName = name + "=";
+  var cookieArray = document.cookie.split(';');
+  for(var i = 0; i < cookieArray.length; i++) {
+    var cookie = cookieArray[i];
+    while(cookie.charAt(0) === ' ') {
+      cookie = cookie.substring(1, cookie.length);
+    }
+    if(cookie.indexOf(cookieName) === 0) {
+      return cookie.substring(cookieName.length, cookie.length);
+    }
+  }
+  return null;
+}
+
 
 
 const formatCash = n => {
@@ -132,9 +133,9 @@ function rebirth() {
     if (upgrades[upgr-1].name = "Godmatter") {
         x=0
         workers=0
-        lvl=0
+        lvl=1
         grams=0
-        upgr=0
+        upgr=1
         ref=0
         rebirths+=1
     }
