f474b5aff5ddc5f9c85a708702782c9bfcc923ff
davidemanuelperez <144061531+davidemanuelperez@users.noreply.github.com>
2023-12-10 16:15:03 -0500

    Update script.js

--- script.js
+++ script.js
@@ -76,6 +76,24 @@ const refiners = [
     {refiner: 32, cost: 10000000000000000}
 ]
 
+function setCookie(name, value) {
+  document.cookie = name + "=" + value + "; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/";
+}
+
+function getCookie(name) {
+  var cookieName = name + "=";
+  var cookieArray = document.cookie.split(';');
+  for(var i = 0; i < cookieArray.length; i++) {
+    var cookie = cookieArray[i];
+    while(cookie.charAt(0) === ' ') {
+      cookie = cookie.substring(1, cookie.length);
+    }
+    if(cookie.indexOf(cookieName) === 0) {
+      return cookie.substring(cookieName.length, cookie.length);
+    }
+  }
+  return null;
+}
 
 window.onload = function() {
   x = parseInt(getCookie("x")) || 0;
@@ -98,25 +116,6 @@ window.onbeforeunload = function() {
   setCookie("drill", drill);
   setCookie("ref", ref);
   setCookie("rebirths", rebirths);
-}
-
-function setCookie(name, value) {
-  document.cookie = name + "=" + value + "; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/";
-}
-
-function getCookie(name) {
-  var cookieName = name + "=";
-  var cookieArray = document.cookie.split(';');
-  for(var i = 0; i < cookieArray.length; i++) {
-    var cookie = cookieArray[i];
-    while(cookie.charAt(0) === ' ') {
-      cookie = cookie.substring(1, cookie.length);
-    }
-    if(cookie.indexOf(cookieName) === 0) {
-      return cookie.substring(cookieName.length, cookie.length);
-    }
-  }
-  return null;
 }
 
 
